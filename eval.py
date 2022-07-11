8a46f7c4348a40a28fcea8310a7a45858f4fb5ca
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-11 12:19:15 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -18,3 +18,20 @@ def Metrics(gt,pred):
     IOU = np.count_nonzero(intersection) / np.count_nonzero(union)
     Dice_coeff = np.count_nonzero(intersection) / (np.count_nonzero(gt) + np.count_nonzero(pred))
     return((IOU,Dice_coeff))
+
+
+class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
