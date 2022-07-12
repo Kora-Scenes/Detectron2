99c872448374c636737b7b0d8bf1fdfe0d221bcf
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-12 11:08:10 +0530

    Update Mrcnn.py

--- Mrcnn.py
+++ Mrcnn.py
@@ -31,7 +31,7 @@ im = cv2.imread(img)
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
