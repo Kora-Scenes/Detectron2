49b0d2e542b955a20c76ef7c546977de562c14b2
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-12 11:21:16 +0530

    Update masks.py

--- masks.py
+++ masks.py
@@ -4,7 +4,6 @@ path_instance = path + '/instance'
 path_semantic = path + '/semantic_rgb'
 
 import glob
-import warnings as wr
 import numpy as np
 import os, json, cv2, random
 
@@ -20,11 +19,13 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 
 from torch import logical_and
 
+import warnings as wr
 wr.filterwarnings("ignore")
 
 gt_masks_cars = []
 gt_masks_persons = []
 c = 1
+
 def Metrics(gt,pred):
     intersection = np.logical_and(gt,pred)
     union = np.logical_or(gt,pred)
@@ -53,6 +54,7 @@ c =1
 it = 0
 im_predmasks_cars = []
 im_predmasks_persons = []
+
 for imageName in sorted(glob.glob(os.path.join(path_images, '*.png'))):
     im = cv2.imread(imageName)
     outputs = predictor(im)
@@ -74,6 +76,7 @@ for imageName in sorted(glob.glob(os.path.join(path_images, '*.png'))):
     for j in out_persons:
         pred_mask_persons = np.logical_or(pred_mask_persons,j)
     im_predmasks_persons.append(pred_mask_persons)
+    
     cv2.imshow('image', im)
     cv2.imshow('pred_mask', pred_mask_cars.astype(np.uint8)*255)
     cv2.imshow('gt',gt_masks_cars[it].astype(np.uint8)*255)
@@ -81,13 +84,14 @@ for imageName in sorted(glob.glob(os.path.join(path_images, '*.png'))):
     cv2.imshow('pred_mask', pred_mask_persons.astype(np.uint8)*255)
     cv2.imshow('gt',gt_masks_persons[it].astype(np.uint8)*255)
     cv2.waitKey(0)
+    
     it += 1
     if c == 10:
         break
 
-
 car = []
 person = []
+
 for i in range(len(gt_masks_persons)):
     gt_cars = gt_masks_cars[i]
     gt_persons = gt_masks_persons[i]
@@ -100,6 +104,7 @@ print('Cars\n','(IOU,DSC)')
 for i in car:
     print(i)
 print('\n')
+
 print('Persons\n','(IOU,DSC)')
 for i in person:
     print(i)
