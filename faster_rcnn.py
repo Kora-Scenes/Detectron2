e463c52833b33e83fc8dba959a7b91153092af8a
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-13 09:41:29 +0530

    Update faster_rcnn.py

--- faster_rcnn.py
+++ faster_rcnn.py
@@ -16,13 +16,9 @@ import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
-# import some common libraries
 import numpy as np
 import os, json, cv2, random
-# from google.colab.patches 
-# import cv2_imshow
 
-# import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
@@ -33,10 +29,8 @@ img = '/home/yln1kor/nikhil-test/cycle.jpg'
 im = cv2.imread(img)
 
 cfg = get_cfg()
-# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
@@ -49,7 +43,6 @@ v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale
 for i in pred:
     out = v.draw_instance_predictions(outputs["instances"][outputs['instances'].pred_classes == i].to("cpu"))
 # out = v.draw_instance_predictions(outputs["instances"][outputs['instances'].pred_classes == 0].to("cpu"))
-# cv2.imshow(out.get_image()[:, :, ::-1])
 out_path = '/home/yln1kor/nikhil-test/Detectron2/cycle_faster.jpg'
 cv2.imwrite(out_path,out.get_image())
 cv2.imshow("output",out.get_image())
