1ae9743a38055b8b2ca908bd8b87671392da4af2
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-13 13:50:06 +0530

    Update faster_rcnn.py

--- faster_rcnn.py
+++ faster_rcnn.py
@@ -1,6 +1,6 @@
 # !python -m pip install pyyaml==5.1
-# # Detectron2 has not released pre-built binaries for the latest pytorch (https://github.com/facebookresearch/detectron2/issues/4053)
-# # so we install from source instead. This takes a few minutes.
+# Detectron2 has not released pre-built binaries for the latest pytorch (https://github.com/facebookresearch/detectron2/issues/4053)
+# so we install from source instead. This takes a few minutes.
 # !python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 # # Install pre-built detectron2 that matches pytorch version, if released:
 # # See https://detectron2.readthedocs.io/tutorials/install.html for instructions
