aefb16a7a5b9a7fb4e26634f00499ec9e1c69073
NikhilAdyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2022-07-13 09:42:00 +0530

    Update faster_rcnn.py

--- faster_rcnn.py
+++ faster_rcnn.py
@@ -30,7 +30,7 @@ im = cv2.imread(img)
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
